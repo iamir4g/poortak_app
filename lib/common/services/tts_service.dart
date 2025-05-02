f54641dbfbdd3c198f3bd3ec8a105e485d919b59
amir.f <amirfarahani10@gmail.com>
2025-05-02 15:29:57 +0330

    feat: :sparkles: show and hide translation in converstion, play voice conversation

--- lib/common/services/tts_service.dart
+++ lib/common/services/tts_service.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_tts/flutter_tts.dart';
+import 'dart:async';
 
 class TTSService {
   final FlutterTts _flutterTts = FlutterTts();
   bool _isInitialized = false;
+  Completer<void>? _speechCompleter;
 
   Future<void> initialize() async {
     if (_isInitialized) return;
@@ -11,6 +13,15 @@ class TTSService {
     await _flutterTts.setSpeechRate(0.5);
     await _flutterTts.setVolume(1.0);
     await _flutterTts.setPitch(0.3);
+    await _flutterTts.awaitSpeakCompletion(true);
+
+    _flutterTts.setCompletionHandler(() {
+      _speechCompleter?.complete();
+    });
+
+    _flutterTts.setErrorHandler((msg) {
+      _speechCompleter?.completeError(msg);
+    });
 
     _isInitialized = true;
   }
@@ -33,10 +44,14 @@ class TTSService {
     if (!_isInitialized) {
       await initialize();
     }
+
+    _speechCompleter = Completer<void>();
     await _flutterTts.speak(text);
+    await _speechCompleter?.future;
   }
 
   Future<void> stop() async {
+    _speechCompleter?.complete();
     await _flutterTts.stop();
   }
 
--- lib/featueres/fetures_sayareh/screens/converstion_screen.dart
+++ lib/featueres/fetures_sayareh/screens/converstion_screen.dart
@@ -11,7 +11,7 @@ import 'package:poortak/featueres/fetures_sayareh/presentation/bloc/converstion_
 // import 'package:poortak/featueres/fetures_sayareh/presentation/bloc/converstion_event.dart';
 // import 'package:poortak/featueres/fetures_sayareh/presentation/bloc/converstion_state.dart';
 
-class ConversationScreen extends StatelessWidget {
+class ConversationScreen extends StatefulWidget {
   static const routeName = "/conversation_screen";
   final String conversationId;
 
@@ -21,11 +21,76 @@ class ConversationScreen extends StatelessWidget {
   });
 
   @override
+  State<ConversationScreen> createState() => _ConversationScreenState();
+}
+
+class _ConversationScreenState extends State<ConversationScreen> {
+  final TTSService ttsService = locator<TTSService>();
+  bool isPlaying = false;
+  int currentPlayingIndex = 0;
+  List<Datum>? sortedMessages;
+  bool showTranslations = false;
+
+  Future<void> playAllConversations(List<Datum> messages) async {
+    if (isPlaying) {
+      await ttsService.stop();
+      setState(() {
+        isPlaying = false;
+        currentPlayingIndex = 0;
+      });
+      return;
+    }
+
+    setState(() {
+      isPlaying = true;
+      currentPlayingIndex = 0;
+    });
+
+    try {
+      for (var i = 0; i < messages.length; i++) {
+        if (!isPlaying) break;
+
+        final message = messages[i];
+        setState(() {
+          currentPlayingIndex = i;
+        });
+
+        // Set voice based on the message's voice field
+        if (message.voice == 'male') {
+          await ttsService.setPitch(0.3);
+        } else if (message.voice == 'female') {
+          await ttsService.setPitch(1.0);
+        }
+
+        // Wait for the current message to finish playing
+        await ttsService.speak(message.text);
+        await Future.delayed(const Duration(
+            milliseconds: 500)); // Add a small delay between messages
+      }
+    } finally {
+      setState(() {
+        isPlaying = false;
+        currentPlayingIndex = 0;
+      });
+    }
+  }
+
+  Future<void> speakText(String text, String voice) async {
+    // Set voice based on the message's voice field
+    if (voice == 'male') {
+      await ttsService.setPitch(0.3);
+    } else if (voice == 'female') {
+      await ttsService.setPitch(1.0);
+    }
+    await ttsService.speak(text);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ConverstionBloc(
         sayarehRepository: locator(),
-      )..add(GetConversationEvent(id: conversationId)),
+      )..add(GetConversationEvent(id: widget.conversationId)),
       child: Scaffold(
         backgroundColor: MyColors.secondaryTint4,
         appBar: AppBar(
@@ -39,6 +104,40 @@ class ConversationScreen extends StatelessWidget {
             style: MyTextStyle.textMatn16,
           ),
         ),
+        bottomNavigationBar: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  if (sortedMessages != null) {
+                    playAllConversations(sortedMessages!);
+                  }
+                },
+                icon: Icon(
+                  isPlaying ? Icons.stop_circle : Icons.play_circle,
+                  size: 30,
+                  color: isPlaying ? Colors.red : Colors.green,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    showTranslations = !showTranslations;
+                  });
+                },
+                icon: Icon(
+                  Icons.translate,
+                  color: showTranslations ? Colors.blue : Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
         body: BlocBuilder<ConverstionBloc, ConverstionState>(
           builder: (context, state) {
             if (state is ConverstionLoading) {
@@ -50,6 +149,8 @@ class ConversationScreen extends StatelessWidget {
             }
 
             if (state is ConverstionSuccess) {
+              sortedMessages = state.data.data
+                ..sort((a, b) => a.order.compareTo(b.order));
               return _buildConversationList(context, state.data);
             }
 
@@ -61,21 +162,13 @@ class ConversationScreen extends StatelessWidget {
   }
 
   Widget _buildConversationList(BuildContext context, ConversationModel data) {
-    final TTSService ttsService = locator<TTSService>();
     final ScrollController scrollController = ScrollController();
-
-    Future<void> speakText(String text, String voice) async {
-      // Set voice based on the message's voice field
-      if (voice == 'male') {
-        await ttsService.setPitch(0.3);
-      } else if (voice == 'female') {
-        await ttsService.setPitch(1.0);
-      }
-      await ttsService.speak(text);
-    }
 
     Widget buildMessageBubble(Datum message) {
       final isFirstPerson = message.voice == 'male';
+      final isCurrentPlaying = sortedMessages != null &&
+          currentPlayingIndex < sortedMessages!.length &&
+          sortedMessages![currentPlayingIndex].id == message.id;
 
       return Align(
         alignment: isFirstPerson ? Alignment.centerRight : Alignment.centerLeft,
@@ -85,6 +178,9 @@ class ConversationScreen extends StatelessWidget {
           decoration: BoxDecoration(
             color: isFirstPerson ? MyColors.primary : Colors.grey[300],
             borderRadius: BorderRadius.circular(20),
+            border: isCurrentPlaying
+                ? Border.all(color: Colors.green, width: 2)
+                : null,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -95,44 +191,28 @@ class ConversationScreen extends StatelessWidget {
                   color: isFirstPerson ? Colors.white : Colors.black,
                 ),
               ),
-              const SizedBox(height: 4),
-              Text(
-                message.translation,
-                style: TextStyle(
-                  color: isFirstPerson ? Colors.white70 : Colors.black54,
-                  fontSize: 12,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.volume_up,
-                      color: isFirstPerson ? Colors.white : Colors.black,
-                      size: 20,
-                    ),
-                    onPressed: () => speakText(message.text, message.voice),
+              if (showTranslations) ...[
+                const SizedBox(height: 4),
+                Text(
+                  message.translation,
+                  style: TextStyle(
+                    color: isFirstPerson ? Colors.white70 : Colors.black54,
+                    fontSize: 12,
                   ),
-                ],
-              ),
+                ),
+              ],
             ],
           ),
         ),
       );
     }
 
-    // Sort messages by order
-    final sortedMessages = data.data
-      ..sort((a, b) => a.order.compareTo(b.order));
-
     return ListView.builder(
       controller: scrollController,
       padding: const EdgeInsets.all(16),
-      itemCount: sortedMessages.length,
+      itemCount: sortedMessages?.length ?? 0,
       itemBuilder: (context, index) {
-        final message = sortedMessages[index];
+        final message = sortedMessages![index];
         return buildMessageBubble(message);
       },
     );
