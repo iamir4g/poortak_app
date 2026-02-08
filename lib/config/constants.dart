71179ab4305ff8d423de21e39c317efe8252b766
amir.f <amirfarahani10@gmail.com>
2026-02-09 00:31:04 +0330

    fix: :bug:

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -9,4 +9,5 @@ class Constants {
 // static const buildVideoAd = "6301d8bc9e17585c89adbf03";
 // static const singleRuneBannerAd = "6301e0d28f178c2d6268df95";
 // static const itemsBannerAd = "6301ef6e8f178c2d6268dfa3";
+  static const smsSignature = "FA+9qCX9VSu";
 }
