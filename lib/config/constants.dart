a79f110ddba91455c171ea7b0245f0c16e54d8f3
amir.f <amirfarahani10@gmail.com>
2026-09-19 19:48:19 +0330

    feat: update base URL in constants to use the production API endpoint; comment out the previous local development URL for clarity

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
-  static String get baseUrl => "http://192.168.40.189:3000/api/v1/";
+  // static String get baseUrl => "http://192.168.40.189:3000/api/v1/";
+  static String get baseUrl => "https://api.poortak.ir/api/v1/";
   static const smsSignature = "FA+9qCX9VSu";
 }
